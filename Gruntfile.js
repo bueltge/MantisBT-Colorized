8e0c60d105eeca1177a5b8aae51076b9fc0da724
bueltge <frank@bueltge.de>
2017-01-13 23:31:39 +0100

    Add csslint include custom options

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,11 +16,23 @@ module.exports = function( grunt ) {
 					'colorized.css': [ 'assets/css/default.min.css', 'assets/css/open-sans.min.css', 'assets/css/buttons.min.css', 'assets/css/colorized.min.css' ]
 				}
 			}
+		},
+		csslint: {
+			strict: {
+				src: ['assets/css/default.css']
+			},
+			lax: {
+				options: {
+					csslintrc: '.csslintrc'
+				},
+				src: ['assets/css/default.css']
+			}
 		}
 	} );
 
 	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks('grunt-contrib-csslint');
 
 	// Default task(s).
-	grunt.registerTask( 'default', [ 'cssmin' ] );
+	grunt.registerTask( 'default', [ 'cssmin', 'csslint:lax' ] );
 };
